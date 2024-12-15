4eaa59396c7a1207e353b111e22852b1f0d36d47
羿玹/Eric <164621612+Ericxpert@users.noreply.github.com>
2024-12-15 15:56:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 cal=  lambda x1,y1,x2,y2: int((abs(x1-x2)+abs(y1-y2)+abs(x1+y1-x2-y2))/2)
+
+
+def cal_list (a,b):
+  k=[]
+  for i in a:
+    k.append(i)
+  for i in b:
+    k.append(i)
+  return cal(k[0],k[1],k[2],k[3])
